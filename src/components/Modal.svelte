50fc9574840dc1238c8ab6af9613c4dc64c10839
chunchunmaru <personal@ks-kao.com>
2024-02-11 22:16:11 +0800

    Added headless ui and modal component

---
+++ src/components/Modal.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { createDialog } from 'svelte-headlessui';
+	import Transition from 'svelte-transition';
+
+	const dialog = createDialog();
+</script>
+
+<slot name="button" {dialog} />
+<div class="relative z-10">
+	<Transition show={$dialog.expanded}>
+		<div class="fixed inset-0 overflow-y-auto bg-gray-950/90">
+			<div class="flex min-h-full items-center justify-center p-4 text-center">
+				<Transition
+					enter="ease-out duration-300"
+					enterFrom="opacity-0 scale-95"
+					enterTo="opacity-100 scale-100"
+					leave="ease-in duration-200"
+					leaveFrom="opacity-100 scale-100"
+					leaveTo="opacity-0 scale-95"
+				>
+					<div
+						class="w-full max-w-md transform overflow-hidden rounded-2xl bg-gray-800 p-6 text-left align-middle shadow-xl transition-all"
+						use:dialog.modal
+					>
+						<slot {dialog} />
+					</div>
+				</Transition>
+			</div>
+		</div>
+	</Transition>
+</div>
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
+	import Modal from './Modal.svelte';
+	import { Button, GradientButton } from 'flowbite-svelte';
 </script>
 
-<nav class="flex h-24 w-full justify-between p-8">
-	<div class="flex gap-6">
-		<img src="./favicon.png" alt="logo" class="h-10 w-10" />
+<nav class="flex h-28 w-full items-center justify-between p-8">
+	<div class="flex items-center gap-6">
+		<img src="./favicon.png" alt="logo" class="h-14 w-14" />
 		<h1 class="text-4xl font-bold text-white">Stella</h1>
+	</div>
+	<div class="flex gap-2">
+		<Modal>
+			<Button class="h-10 w-10 p-0" let:dialog slot="button" on:click={dialog.open}>
+				<Icon icon="fa6-solid:info" width={20} height={20} />
+			</Button>
+			<svelte:fragment let:dialog>
+				<h3>Info</h3>
+				<Button on:click={dialog.close}>Close</Button>
+			</svelte:fragment>
+		</Modal>
+		<Modal>
+			<Button class="h-10 w-10 p-0" let:dialog slot="button" on:click={dialog.open}>
+				<Icon icon="fa6-solid:gear" width={20} height={20} />
+			</Button>
+			<svelte:fragment let:dialog>
+				<h3>Settings</h3>
+				<Button on:click={dialog.close}>Close</Button>
+			</svelte:fragment>
+		</Modal>
 	</div>
 </nav>
