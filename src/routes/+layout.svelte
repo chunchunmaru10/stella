62cd1f5921e53c8f3f671046361328209a08e32a
chunchunmaru <personal@ks-kao.com>
2024-02-14 10:46:51 +0800

    Added toast

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script>
+	import { toast } from '$lib/stores/toast';
 	import '../app.css';
 	import Nav from '../components/Nav.svelte';
+	import Toaster from '../components/Toaster.svelte';
 </script>
 
 <svelte:head>
 	<title>Stella</title>
 </svelte:head>
+<Toaster />
 <Nav />
 <main class="mb-8 md:h-[calc(100svh-7rem-2rem)]">
 	<slot />
