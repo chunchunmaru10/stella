07107b252907fb93731bf7d02a069483b37abc98
chunchunmaru <personal@ks-kao.com>
2024-03-02 09:06:09 +0800

    Sorted characters in settings page

--- src/components/CharacterToggleSettings.svelte
+++ src/components/CharacterToggleSettings.svelte
@@ -12,7 +12,8 @@
 	<h2 class="my-4 text-xl font-semibold">Included Characters</h2>
 	<div class="flex flex-wrap gap-4 pb-4">
 		{#await data then characters}
-			{#each characters as character}
+			{@const sorted = characters.sort((a, b) => a.name.localeCompare(b.name))}
+			{#each sorted as character}
 				<!-- only render if user wants leaks or character release date is before now -->
 				{#if $settings.includeUnreleaseCharacters || character.releaseDate.getTime() < new Date().getTime()}
 					<CharacterToggle {character} />
